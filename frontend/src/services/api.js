5b44e44157d65bd82a06c62dca93a87386f3db47
Prple69 <prple666@mail.ru>
2026-02-22 06:00:27 +0700

    new url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://gameyourlifetwa.vercel.app/api',
+  baseURL: 'https://gameurlife.ru.tuna.am/api',
 });
 
 export const userService = {
