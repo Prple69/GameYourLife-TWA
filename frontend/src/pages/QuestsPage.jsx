65d9b57a789cfae44dedf37ba64455834985685b
Prple69 <prple666@mail.ru>
2026-02-22 15:21:51 +0700

    fix

--- frontend/src/pages/QuestsPage.jsx
+++ frontend/src/pages/QuestsPage.jsx
@@ -48,7 +48,7 @@ const QuestsPage = ({
         <Header 
           title="Задания" 
           subtitle="Доска активных контрактов" 
-          pt="pt-[60px]"
+          pt="pt-[72px]"
         />
 
         <div className="flex-1 overflow-y-auto space-y-4 pr-1">
