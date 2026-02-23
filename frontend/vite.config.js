65f32facde0d8fb4c1fd4cf118839af6a47e261a
Prple69 <prple666@mail.ru>
2026-02-24 00:00:48 +0700

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Все запросы к /api будут перенаправлены на твой бэк на 8000 порту
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
